0c0f4a8ed5c7f352d322821a5a9242858ba6413f
Kevin Deisz <kevin.deisz@gmail.com>
2020-06-15 11:36:58 -0400

    Initial commit

---
+++ contentScript.js
@@ -0,0 +1,73 @@
+// Paths taken from font-awesome (https://fontawesome.com/license)
+var statusIcons = {
+  diamond: {
+    viewBox: "0 0 576 512",
+    path: "M464 0H112c-4 0-7.8 2-10 5.4L2 152.6c-2.9 4.4-2.6 10.2.7 14.2l276 340.8c4.8 5.9 13.8 5.9 18.6 0l276-340.8c3.3-4.1 3.6-9.8.7-14.2L474.1 5.4C471.8 2 468.1 0 464 0zm-19.3 48l63.3 96h-68.4l-51.7-96h56.8zm-202.1 0h90.7l51.7 96H191l51.6-96zm-111.3 0h56.8l-51.7 96H68l63.3-96zm-43 144h51.4L208 352 88.3 192zm102.9 0h193.6L288 435.3 191.2 192zM368 352l68.2-160h51.4L368 352z"
+  },
+  github: {
+    viewBox: "0 0 496 512",
+    path: "M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"
+  },
+  heartbeat: {
+    viewBox: "0 0 512 512",
+    path: "M320.2 243.8l-49.7 99.4c-6 12.1-23.4 11.7-28.9-.6l-56.9-126.3-30 71.7H60.6l182.5 186.5c7.1 7.3 18.6 7.3 25.7 0L451.4 288H342.3l-22.1-44.2zM473.7 73.9l-2.4-2.5c-51.5-52.6-135.8-52.6-187.4 0L256 100l-27.9-28.5c-51.5-52.7-135.9-52.7-187.4 0l-2.4 2.4C-10.4 123.7-12.5 203 31 256h102.4l35.9-86.2c5.4-12.9 23.6-13.2 29.4-.4l58.2 129.3 49-97.9c5.9-11.8 22.7-11.8 28.6 0l27.6 55.2H481c43.5-53 41.4-132.3-7.3-182.1z"
+  }
+};
+
+// Make an SVG icon for a status
+function makeStatusIcon (status) {
+  var statusIcon = statusIcons[status.icon];
+
+  var svgNode = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+  var pathNode = document.createElementNS("http://www.w3.org/2000/svg", "path");
+
+  svgNode.setAttribute("class", "rtb--icon");
+  svgNode.setAttribute("aria-hidden", true);
+  svgNode.setAttribute("role", "img");
+  svgNode.setAttribute("viewBox", statusIcon.viewBox);
+
+  pathNode.setAttribute("fill", "currentColor");
+  pathNode.setAttribute("d", statusIcon.path);
+  svgNode.appendChild(pathNode);
+
+  return svgNode;
+}
+
+// Make a node representing the status from the ruby toolbox
+function makeStatusTag (status) {
+  var spanNode = document.createElement("span");
+  spanNode.className = "rtb--tag rtb--tag-" + status.level;
+
+  spanNode.appendChild(makeStatusIcon(status));
+  spanNode.appendChild(document.createTextNode(status.label));
+
+  return spanNode;
+}
+
+// Make a node containing all of the rendered status tags
+function makeStatusTags (response) {
+  var divNode = document.createElement("div");
+  divNode.className = "rtb--tags";
+
+  response.projects[0].health.statuses.forEach(function (status) {
+    divNode.appendChild(makeStatusTag(status));
+  });
+
+  return divNode;
+}
+
+// Fire off an AJAX request to get the statuses, then render them
+var xhr = new XMLHttpRequest();
+
+xhr.onreadystatechange = function () {
+  if ((xhr.readyState === 4) && (xhr.status >= 200) && (xhr.status < 300)) {
+    var statusTags = makeStatusTags(JSON.parse(xhr.responseText));
+    document.querySelector(".page__heading").appendChild(statusTags);
+  }
+};
+
+var pathname = document.location.pathname;
+var gemName = pathname.slice(pathname.lastIndexOf("/") + 1);
+
+xhr.open("GET", "https://www.ruby-toolbox.com/api/projects/compare/" + gemName);
+xhr.send();

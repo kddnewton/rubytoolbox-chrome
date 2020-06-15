e013bb9f1a7ffa466b14337509663d7da1a3dc87
Kevin Deisz <kevin.deisz@gmail.com>
2020-06-15 12:10:30 -0400

    Why was I supporting IE? This is a chrome extension

--- contentScript.js
+++ contentScript.js
@@ -1,5 +1,5 @@
 // Paths taken from font-awesome (https://fontawesome.com/license)
-var statusIcons = {
+const statusIcons = {
   diamond: {
     viewBox: "0 0 576 512",
     path: "M464 0H112c-4 0-7.8 2-10 5.4L2 152.6c-2.9 4.4-2.6 10.2.7 14.2l276 340.8c4.8 5.9 13.8 5.9 18.6 0l276-340.8c3.3-4.1 3.6-9.8.7-14.2L474.1 5.4C471.8 2 468.1 0 464 0zm-19.3 48l63.3 96h-68.4l-51.7-96h56.8zm-202.1 0h90.7l51.7 96H191l51.6-96zm-111.3 0h56.8l-51.7 96H68l63.3-96zm-43 144h51.4L208 352 88.3 192zm102.9 0h193.6L288 435.3 191.2 192zM368 352l68.2-160h51.4L368 352z"
@@ -14,60 +14,42 @@ var statusIcons = {
   }
 };
 
-// Make an SVG icon for a status
-function makeStatusIcon (status) {
-  var statusIcon = statusIcons[status.icon];
+const makeStatusIcon = (status) => {
+  const { viewBox, path } = statusIcons[status.icon];
 
-  var svgNode = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-  var pathNode = document.createElementNS("http://www.w3.org/2000/svg", "path");
+  const svgNode = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+  const pathNode = document.createElementNS("http://www.w3.org/2000/svg", "path");
 
   svgNode.setAttribute("class", "rtb--icon");
   svgNode.setAttribute("aria-hidden", true);
   svgNode.setAttribute("role", "img");
-  svgNode.setAttribute("viewBox", statusIcon.viewBox);
+  svgNode.setAttribute("viewBox", viewBox);
 
   pathNode.setAttribute("fill", "currentColor");
-  pathNode.setAttribute("d", statusIcon.path);
+  pathNode.setAttribute("d", path);
   svgNode.appendChild(pathNode);
 
   return svgNode;
-}
-
-// Make a node representing the status from the ruby toolbox
-function makeStatusTag (status) {
-  var spanNode = document.createElement("span");
-  spanNode.className = "rtb--tag rtb--tag-" + status.level;
-
-  spanNode.appendChild(makeStatusIcon(status));
-  spanNode.appendChild(document.createTextNode(status.label));
-
-  return spanNode;
-}
-
-// Make a node containing all of the rendered status tags
-function makeStatusTags (response) {
-  var divNode = document.createElement("div");
-  divNode.className = "rtb--tags";
-
-  response.projects[0].health.statuses.forEach(function (status) {
-    divNode.appendChild(makeStatusTag(status));
-  });
-
-  return divNode;
-}
-
-// Fire off an AJAX request to get the statuses, then render them
-var xhr = new XMLHttpRequest();
-
-xhr.onreadystatechange = function () {
-  if ((xhr.readyState === 4) && (xhr.status >= 200) && (xhr.status < 300)) {
-    var statusTags = makeStatusTags(JSON.parse(xhr.responseText));
-    document.querySelector(".page__heading").appendChild(statusTags);
-  }
 };
 
-var pathname = document.location.pathname;
-var gemName = pathname.slice(pathname.lastIndexOf("/") + 1);
+const { pathname } = document.location;
+const gemName = pathname.slice(pathname.lastIndexOf("/") + 1);
 
-xhr.open("GET", "https://www.ruby-toolbox.com/api/projects/compare/" + gemName);
-xhr.send();
+fetch(`https://www.ruby-toolbox.com/api/projects/compare/${gemName}`)
+  .then((response) => response.json())
+  .then((response) => {
+    const divNode = document.createElement("div");
+    divNode.className = "rtb--tags";
+
+    response.projects[0].health.statuses.forEach((status) => {
+      const spanNode = document.createElement("span");
+      spanNode.className = `rtb--tag rtb--tag-${status.level}`;
+
+      spanNode.appendChild(makeStatusIcon(status));
+      spanNode.appendChild(document.createTextNode(status.label));
+
+      divNode.appendChild(spanNode);
+    });
+
+    document.querySelector(".page__heading").appendChild(divNode);
+  });
